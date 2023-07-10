e7a03bd59a71625d286876d888167210c48f5fc8
Vitaly Turovsky <vital2580@icloud.com>
2023-07-10 03:27:56 +0300

    setup vite add pause & game menu, add mouse & gamepad support, ci

--- src/index.ts
+++ src/index.ts
@@ -14,12 +14,12 @@ const BALL_PLAYER_RADIUS = 30
 const SPACE = 250
 const NEW_OBJECT_INTERVAL = 3_000
 
-const objects: Array<{x: number, y: number}> = []
+const objects: Array<{ x: number; y: number }> = []
 
 setInterval(() => {
     objects.push({
         x: canvas.width,
-        y: Math.random() > 0.5 ? 0 : -1
+        y: Math.random() > 0.5 ? 0 : -1,
     })
 }, NEW_OBJECT_INTERVAL)
 
@@ -30,33 +30,63 @@ let ballY: number
 let ballState = 2
 let score = 0
 
-const userToggleBallState = () => {
+let prevUpdateCoordsTime = 0
+let paused = false
+
+const ballInput = () => {
+    if (paused) {
+        prevUpdateCoordsTime = performance.now()
+        paused = false
+        requestAnimationFrame(renderFrame)
+        return
+    }
     if (Math.abs(ballState) === 1) return
     ballState = ballState === 2 ? -1 : 1
 }
 
 addEventListener('keydown', ({ code }) => {
     if (code === 'Space') {
-        userToggleBallState()
+        ballInput()
     }
 })
 
-addEventListener('touchstart', () => {
-    userToggleBallState()
+addEventListener('pointerdown', ({ target }) => {
+    if ((target as HTMLElement).tagName.toLowerCase() !== 'canvas') return
+    ballInput()
 })
 
-const renderPlayer = (color) => {
+const connectedGamepads = [] as Gamepad[]
+
+let pullGamepadsInterval: number
+const gamepadConnected = () => {
+    pullGamepadsInterval = setInterval(() => {
+        for (const [i, gamepad] of connectedGamepads.entries()) {
+            if (gamepad.buttons) {
+                ballInput()
+            }
+        }
+    }, 100)
+}
+addEventListener('gamepadconnected', ({ gamepad }) => {
+    connectedGamepads.push(gamepad)
+
+    gamepadConnected()
+})
+removeEventListener('gamepaddisconnected', ({ gamepad }) => {
+    connectedGamepads.splice(connectedGamepads.indexOf(gamepad), 1)
+
+    if (connectedGamepads.length === 0) {
+        clearInterval(pullGamepadsInterval)
+    }
+})
+
+const renderPlayer = (color: string | CanvasGradient | CanvasPattern) => {
     ctx.fillStyle = color
     ctx.beginPath()
     ctx.arc(PLAYER_X, ballY, BALL_PLAYER_RADIUS, 0, Math.PI * 2)
     ctx.fill()
     ctx.strokeStyle = color
-    ctx.strokeRect(
-        PLAYER_X - BALL_PLAYER_RADIUS,
-        ballY - BALL_PLAYER_RADIUS,
-        BALL_PLAYER_RADIUS * 2,
-        BALL_PLAYER_RADIUS * 2
-    )
+    ctx.strokeRect(PLAYER_X - BALL_PLAYER_RADIUS, ballY - BALL_PLAYER_RADIUS, BALL_PLAYER_RADIUS * 2, BALL_PLAYER_RADIUS * 2)
 }
 
 const renderEdges = () => {
@@ -69,25 +99,42 @@ const renderEdges = () => {
 const renderObjects = () => {
     ctx.fillStyle = 'red'
     for (const object of objects) {
-        ctx.fillRect(
-            object.x,
-            object.y < 0 ? bottomEdge - OBJECT_SIZE : topEdge,
-            OBJECT_SIZE,
-            OBJECT_SIZE
-        )
+        ctx.fillRect(object.x, object.y < 0 ? bottomEdge - OBJECT_SIZE : topEdge, OBJECT_SIZE, OBJECT_SIZE)
     }
 }
 
-const isIntersects = (x1Start, x1End, x2Start, x2End) => {
+const isIntersects = (x1Start: number, x1End: number, x2Start: number, x2End: number) => {
     return x1Start <= x2End && x1End >= x2Start
 }
 
 const SPEED_FACTOR = 0.3
 const SPEED_OBJECTS_FACTOR = 1
 
-let prevUpdateCoordsTime = 0
+const pauseOrEndGame = (isGameOver: boolean) => {
+    paused = true
+    let fontSize = 60
+    ctx.font = `${fontSize}px sans-serif`
+    ctx.fillStyle = isGameOver ? 'red' : 'lightgreen'
+    let printText = isGameOver ? 'Game Over' : 'Paused'
+    ctx.fillText(printText, (canvas.width - ctx.measureText(printText).width) / 2, center - fontSize / 2)
 
-const updateCoords = (timestamp) => {
+    fontSize = 30
+    ctx.font = `${fontSize}px sans-serif`
+    printText = 'Click or press space to continue'
+    ctx.fillText(printText, (canvas.width - ctx.measureText(printText).width) / 2, center + fontSize / 2)
+
+    if (isGameOver) {
+        objects.splice(0, objects.length)
+        score = 0
+    }
+}
+
+window.addEventListener('blur', () => {
+    if (paused) return
+    pauseOrEndGame(false)
+})
+
+const updateCoords = (timestamp: number) => {
     center = canvas.height / 2
     bottomEdge = center + SPACE / 2
     topEdge = center - SPACE / 2
@@ -104,9 +151,8 @@ const updateCoords = (timestamp) => {
             isIntersects(y, y + OBJECT_SIZE, ballY - BALL_PLAYER_RADIUS, ballY + BALL_PLAYER_RADIUS) &&
             isIntersects(x, x + OBJECT_SIZE, PLAYER_X - BALL_PLAYER_RADIUS, PLAYER_X + BALL_PLAYER_RADIUS)
         ) {
-            alert('game over')
-            objects.splice(0, objects.length)
-            score = 0
+            pauseOrEndGame(true)
+            return
         }
 
         if (x <= -OBJECT_SIZE) {
@@ -127,8 +173,10 @@ const updateCoords = (timestamp) => {
     }
 }
 
-const renderFrame = (timestamp) => {
+const renderFrame = (timestamp: DOMHighResTimeStamp) => {
+    if (paused) return
     updateCoords(timestamp)
+    if (paused) return
 
     ctx.clearRect(0, 0, canvas.width, canvas.height)
 
@@ -138,9 +186,13 @@ const renderFrame = (timestamp) => {
     }
     renderPlayer('deepskyblue')
     renderObjects()
-    requestAnimationFrame(renderFrame)
+    ctx.font = '20px sans-serif'
     ctx.fillStyle = 'white'
     ctx.fillText(`Score: ${score}`, 0, 50)
+    requestAnimationFrame(renderFrame)
 }
 
-requestAnimationFrame(renderFrame)
+requestAnimationFrame(time => {
+    renderFrame(time)
+    pauseOrEndGame(false)
+})
